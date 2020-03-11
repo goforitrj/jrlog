d122f711aa447cea57bb5d67f1a04cfd3e9cf420
goforitrj <eleveniasjr@gmail.com>
2020-03-11 16:29:24 +1100

    Set redux to jrlog

---
+++ src/modules/counter.js
@@ -0,0 +1,20 @@
+// action types
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+// action creator functions
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+const initialState = 0;
+
+export default function counter(state = initialState, action) {
+    switch (action.type) {
+        case INCREASE:
+            return state + 1;
+        case DECREASE:
+            return state - 1;
+        default:
+            return state;
+    }
+}
